c4390c0f67ae65be4822674db8a0bba2c8750030
sergey <sergey@sergey-desktop.(none)>
2012-12-26 18:09:15 +0400

    After clone repo perform cap -vT ,parse it and create Tasks

--- app/models/project/clone_repo.rb
+++ app/models/project/clone_repo.rb
@@ -10,6 +10,9 @@ class Project
 
       Strano::Repo.clone project.url
 
+      capvt=project.cap(%w(-vT)).task_list(:all).sort_by(&:fully_qualified_name).inject({}) {|h,t| h[t.fully_qualified_name]=t.desc; h}
+            capvt.each{|name,desc| Task.create name: name, description: desc, project_id: project.id, author_id:1}
+
       Project.update_all({:updated_at => Time.now,
                           :cloned_at => Time.now,
                           :pulled_at => Time.now,
